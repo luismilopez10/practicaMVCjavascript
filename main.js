e86a28007d6341d7151e3ce1cfcb92c7f98979c4
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-04-29 18:39:48 -0500

    Added movement to bars

--- main.js
+++ main.js
@@ -28,14 +28,18 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
+        this.speed = 10;
     }
 
     self.Bar.prototype = {
         down: function(){
-
+            this.y += this.speed;
         },
         up: function(){
-
+            this.y -= this.speed;
+        },
+        toString: function(){
+            return  "x: "+ this.x +"y: "+ this.y;
         }
     }
 })();
@@ -71,15 +75,26 @@
     }
 })();
 
+var board = new Board (800, 400);
+var bar = new Bar(20,100,40,100,board);
+var bar = new Bar(735,100,40,100,board);
+var canvas = document.getElementById('canvas');
+var board_view = new BoardView(canvas,board);
+
+document.addEventListener("keydown", function(ev){
+    console.log(ev.keyCode);
+    if (ev.keyCode == 38) {
+        bar.up();
+    }
+    else if (ev.keyCode == 40) {
+        bar.down();
+    }
+});
+
 window.addEventListener("load", main);
 
 //Controller
 function main(){
-    var board = new Board (800, 400);
-    var bar = new Bar(20,100,40,100,board);
-    var bar = new Bar(735,100,40,100,board);
-    var canvas = document.getElementById('canvas');
-    var board_view = new BoardView(canvas,board);
 
     board_view.draw();
 }
